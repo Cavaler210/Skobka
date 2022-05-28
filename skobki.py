ec320019d2d92f6d851f7502151ac5a07a5cac41
Vladimir <honeyalica210@yandex.ru>
2022-05-28 19:42:49 +0300

    test

--- skobki.py
+++ skobki.py
@@ -1,16 +1,21 @@
-print("Введите скобочную последовательность из '(' и ')' ")
-def  skobka(inp):
+# print("Введите скобочную последовательность из '(' и ')' ")
+
+
+def skobka(inp):
     x = 0
     for i in inp:
         if i == '(':
             x += 1
+
         elif i == ')':
             x -= 1
-            if x == -1:
+            if x << 0:
                 break
+
     if x == 0:
         print('Правильная скобочная последовательность')
     elif x != 0:
         print('неверная последовательность')
-skobka(input())
 
+    #print(x)
+#print(skobka("()"))
--- skobki_test.py
+++ skobki_test.py
@@ -1,8 +1,16 @@
 import unittest
 from skobki import skobka
 
+
 class TestSkobki(unittest.TestCase):
-    def test_positive_sequence(self): #тестируем правильную последовательность
-        call=skobka(input()) # Вызов тестируемой функции
-        result="Верная скобочная последовательность" # Ожидаемый результат
-        self.assertEqual(call, result, skobka("()"))
+    def test_positive_sequence(self):  # тестируем правильную последовательность
+
+        self.assertEqual(skobka("()"), skobka("()"))
+        self.assertEqual(skobka(""), skobka(""))
+
+    def test_not(self):
+        self.assertEqual(skobka(")"), skobka(")"))
+        self.assertEqual(skobka(")("),skobka(")("))
+        self.assertEqual(skobka("(()))"),skobka("(()))"))
+if __name__=="__main__":
+    unittest.main()
